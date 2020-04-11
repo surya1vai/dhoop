3634f106db9975dbb37a8dd73be1eb2e182a0256
surya1vai <63462688+surya1vai@users.noreply.github.com>
2020-04-11 21:51:07 +0545

    Pattern Print
    Pattern print of the flag of Nepal

---
+++ NepalPattern.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int i, j;
+	for(i=6;i>=0;i--)
+	{
+	
+	     for(j=0;j<=i;j++)
+	        printf("\tNEPAL");
+	    printf("\n");
+}
+for(i=1;i<=6;i++)
+	{
+	
+	     for(j=0;j<=i;j++)
+	        printf("\tNEPAL");
+	    printf("\n");
+}
+}
